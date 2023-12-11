694f3776e010956dd5b2b3c6878f242f0da3f09d
yating52997 <yating2357.ee10@nycu.edu.tw>
2023-12-11 20:11:51 +0800

    add gifpy

---
+++ AI_principle_HW3/GAN/gen_gif.py
@@ -0,0 +1,19 @@
+import imageio
+import glob
+import PIL
+# Display a single image using the epoch number
+def display_image(epoch_no):
+    return PIL.Image.open('image_at_epoch_{:04d}.jpg'.format(epoch_no))
+
+
+anim_file = 'flower_small.gif'
+
+with imageio.get_writer(anim_file, mode='I') as writer:
+    filenames = glob.glob('./pic_medium/image*.jpg')
+    filenames = sorted(filenames)
+    for filename in filenames:
+        image = imageio.imread(filename)
+        writer.append_data(image)
+    image = imageio.imread(filename)
+    writer.append_data(image)
+
